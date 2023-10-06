ef815c055fffdaeca55a1bd1a6fb35e992e7ab31
Konrad Banasik <konrad2666@gmail.com>
2023-10-06 21:27:34 +0200

    Add class Box

---
+++ src/Box.java
@@ -0,0 +1,70 @@
+public class Box {
+    private double width;
+    private double height;
+    private  double length;
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @param length
+     */
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+    /**
+     *
+     * @param side returns a Box that is a Cube
+     */
+    Box(double side){
+        this(side,side,side);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    /**
+     *
+     * @return the volume of the Box
+     */
+    public double getVolume(){
+        return (height * width * length);
+    }
+    public double getSurfaceArea(){
+        return ((width * length + length * height + height * width)*2);
+    }
+    public void printBox(){
+        if(width <= 0 || height <= 0 || length <= 0){
+            System.out.println("Box contains invalid attributes");
+        } else {
+            System.out.println("Length = " + length);
+            System.out.println("Height = " + height);
+            System.out.println("Width = " + width);
+            System.out.println("Volume = " + getVolume());
+            System.out.println("Surface area = " + getSurfaceArea());
+        }
+    }
+}
