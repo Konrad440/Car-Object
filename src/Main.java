5cfd49055acfa97f95f93f4fc4b8e9a88c8762f1
Konrad Banasik <konrad2666@gmail.com>
2023-10-06 21:27:52 +0200

    Add class Box

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String name = "Konrad";
+        int length = name.length();
+        String biggerName = name.toUpperCase();
+        String text = "    Jak nauczyć się programowania    ";
+        String text2 = text.replace(" ", "-");
+        String text3 = text.strip();
+        boolean startsWithK = name.startsWith("K");
+
+        System.out.println(name);
+        System.out.println(length);
+        System.out.println(biggerName);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(startsWithK);
     }
 }
