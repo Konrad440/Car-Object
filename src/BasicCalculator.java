821073cde3858356dbb1655b8076a790d535b56f
Konrad Banasik <konrad2666@gmail.com>
2023-10-07 18:52:12 +0200

    Add class BasicCalculator

---
+++ src/BasicCalculator.java
@@ -0,0 +1,14 @@
+public class BasicCalculator {
+    public final double add(double x, double y){
+        return x + y;
+    }
+    public final double subtract(double x, double y){
+        return x - y;
+    }
+    public final double multiply(double x, double y) {
+        return x * y;
+    }
+    public final double divide(double x, double y){
+        return x / y;
+    }
+}
