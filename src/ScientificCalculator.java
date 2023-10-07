f465019113f82004a685de2e8862815c70d1606a
Konrad Banasik <konrad2666@gmail.com>
2023-10-07 18:52:57 +0200

    Add class ScientificCalculator

---
+++ src/ScientificCalculator.java
@@ -0,0 +1,18 @@
+public class ScientificCalculator {
+    public static final double PI = 3.14159;
+
+    private double holdValue;
+
+    public double exp(double e){
+        return e*e;
+    }
+    public double log(double x) {
+        return x;
+    }
+    public void putValueInMemory(double y){
+
+    }
+    public double getValueFromMemory(){
+        return (0);
+    }
+}
