a0df57957648d923d0ed43e6675a3c61a0b77a10
Konrad Banasik <konrad2666@gmail.com>
2023-10-07 18:52:33 +0200

    Add class TrigonometricCalculator

---
+++ src/TrigonometricCalculator.java
@@ -0,0 +1,17 @@
+public class TrigonometricCalculator {
+    public double sine(double x){
+        return(0);
+    }
+    public double cosine(double x){
+        return(0);
+    }
+    public double tangent(double x){
+        return (0);
+    }
+    public double arccosine(double x) {
+        return (0);
+    }
+    public double arctangent(double x){
+        return (0);
+    }
+}
